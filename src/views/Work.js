cc5154f59146343d959ca90574d5eb0572a3f21c
Charlotte Skinner <cs.skinner90@hotmail.co.uk>
2020-08-02 18:06:55 +0100

    Update work image UI

--- src/views/Work.js
+++ src/views/Work.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import LazyImage from '../components/LazyImage'
+import BackgroundImage from '../components/BackgroundImage'
 import './Work.css'
 
 export default ({ workFields }) => {
@@ -8,11 +8,19 @@ export default ({ workFields }) => {
     <div className='Work'>
       <div className="gallery-container">
         {workFields.map(item => (
-          <>
+          <div className="WorkCard">
             {item.galleryImages.map(image => (
-              <LazyImage key={image} src={image} alt='LazyImage' />
+              <div className='WorkCard--Image relative'>
+                <BackgroundImage
+                  src={image}
+                  alt={image}
+                />
+              </div>
             ))}
-          </>
+            <div className='WorkCard--Content'>
+              {item.title && <h3 className='WorkCard--Title'>{item.title}</h3>}
+            </div>
+          </div>
         ))}
       </div>
     </div>
